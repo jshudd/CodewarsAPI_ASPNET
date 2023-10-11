2effd69eae3af8c232b6be661d4829f2c280062b
Jeremy Huddleston <jshudd@gmail.com>
2023-10-11 11:38:36 -0500

    created ApiRepo class; implemented interface

---
+++ CodewarsAPI_ASPNET/ApiRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CodewarsAPI_ASPNET
+{
+	public class ApiRepo : IApiRepo
+	{
+		public ApiRepo()
+		{
+		}
+
+        public string CallAPI(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
