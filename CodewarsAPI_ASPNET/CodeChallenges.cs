476310597233c2b538a04a6e7f0ff48bf7bdafa6
Jeremy Huddleston <jshudd@gmail.com>
2023-10-11 10:31:43 -0500

    created CodeChallenges class

---
+++ CodewarsAPI_ASPNET/CodeChallenges.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CodewarsAPI_ASPNET
+{
+	public class CodeChallenges
+	{
+        public int? TotalAuthored { get; set; }
+        public int? TotalCompleted { get; set; }
+    }
+}
+
