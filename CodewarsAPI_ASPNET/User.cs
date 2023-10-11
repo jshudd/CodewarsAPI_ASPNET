0dd9f597a5472c83b5aada43e6ed07390e90a1e4
Jeremy Huddleston <jshudd@gmail.com>
2023-10-11 10:28:18 -0500

    added User class

---
+++ CodewarsAPI_ASPNET/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CodewarsAPI_ASPNET
+{
+	public class User
+	{
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public int? Honor { get; set; }
+        public string Clan { get; set; }
+        public int? LeaderboardPosition { get; set; }
+        public List<object> Skills { get; set; }
+        public Ranks Ranks { get; set; }
+        public CodeChallenges CodeChallenges { get; set; }
+    }
+}
+
