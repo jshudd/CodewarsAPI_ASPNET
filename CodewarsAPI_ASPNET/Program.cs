1c12c0c0a51c82e4caaaad35f44908e7bfb7ad6c
Jeremy Huddleston <jshudd@gmail.com>
2023-11-02 16:27:46 -0500

    changed starting controller to User

--- CodewarsAPI_ASPNET/Program.cs
+++ CodewarsAPI_ASPNET/Program.cs
@@ -26,7 +26,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=Index}/{id?}");
 
 app.Run();
 
