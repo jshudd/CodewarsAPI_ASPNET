8d65ff8cb3f93c7ea6db9c25ddd000e13520396f
Jeremy Huddleston <jshudd@gmail.com>
2023-11-02 16:16:00 -0500

    created UserController; added Index()

---
+++ CodewarsAPI_ASPNET/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodewarsAPI_ASPNET.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IApiRepo _repo;
+
+        public UserController(IApiRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index(string username)
+        {
+            var userObj = new User();
+
+            if (username == null)
+                return View(userObj);
+
+            // wrap in try-catch after exception found
+            userObj.JSON = _repo.CallApi(username).ToString();
+
+            userObj = _repo.DeserializeJson(userObj);
+
+            return View(userObj);
+        }
+    }
+}
+
