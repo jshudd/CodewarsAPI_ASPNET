6ade99e52e474ca521f15f3fe13f1547f9204e5d
Jeremy Huddleston <jshudd@gmail.com>
2023-11-05 14:45:58 -0600

    created Group model

---
+++ CodewarsAPI_ASPNET/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CodewarsAPI_ASPNET.Models
+{
+	public class Group
+	{
+		public Group()
+		{
+		}
+
+		public Group(List<User> userGroup)
+		{
+			UserGroup = userGroup;
+		}
+
+		public IEnumerable<User> UserGroup { get; set; }
+	}
+}
+
