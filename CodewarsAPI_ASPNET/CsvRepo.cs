a5322083949658f905c48ce2f368fe9ef61970ef
Jeremy Huddleston <jshudd@gmail.com>
2023-11-05 16:42:26 -0600

    implemented interface; added fields; implemented method

--- CodewarsAPI_ASPNET/CsvRepo.cs
+++ CodewarsAPI_ASPNET/CsvRepo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace CodewarsAPI_ASPNET
 {
 	public class CsvRepo : ICsvRepo
@@ -6,6 +7,30 @@ namespace CodewarsAPI_ASPNET
 		public CsvRepo()
 		{
 		}
-	}
+
+        private static string _relativeDirPath = "/wwwroot/csvFiles";
+        private readonly string _dirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _relativeDirPath);
+
+        public IEnumerable<string>? RetrieveCsvFileNames()
+        {
+            var fileNameList = new List<string>();
+
+            if (Directory.Exists(_dirPath))
+            {
+                fileNameList = Directory.GetFiles(_dirPath).ToList();
+
+                for (var i = 0; i < fileNameList.Count(); i++)
+                {
+                    fileNameList[i] = Path.GetFileNameWithoutExtension(fileNameList[i]);
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+            return fileNameList;
+        }
+    }
 }
 
