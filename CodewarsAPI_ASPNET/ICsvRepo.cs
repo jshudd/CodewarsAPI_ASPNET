bb4e78b6a425f6fa4a97f339caaa52934c5b609b
Jeremy Huddleston <jshudd@gmail.com>
2023-11-05 16:26:33 -0600

    created stubbed out RetrieveCsvFileNames()

--- CodewarsAPI_ASPNET/ICsvRepo.cs
+++ CodewarsAPI_ASPNET/ICsvRepo.cs
@@ -3,6 +3,8 @@ namespace CodewarsAPI_ASPNET
 {
 	public interface ICsvRepo
 	{
+		public IEnumerable<string>? RetrieveCsvFileNames();
+		//public bool VerifyVaildFileName(string fileName); // used to prevent duplicate filenames
 	}
 }
 
