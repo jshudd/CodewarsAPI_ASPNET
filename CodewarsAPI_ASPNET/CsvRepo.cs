bc266b4aa5451725d44edcce1f239629034f8961
Jeremy Huddleston <jshudd@gmail.com>
2023-11-05 17:04:53 -0600

    updated filepath for csv files

--- CodewarsAPI_ASPNET/CsvRepo.cs
+++ CodewarsAPI_ASPNET/CsvRepo.cs
@@ -8,16 +8,16 @@ namespace CodewarsAPI_ASPNET
 		{
 		}
 
-        private static string _relativeDirPath = "/wwwroot/csvFiles";
-        private readonly string _dirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _relativeDirPath);
+        private static string _relativeDirPath = "wwwroot/csvFiles";
+        //private readonly string _dirPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _relativeDirPath);
 
         public IEnumerable<string>? RetrieveCsvFileNames()
         {
             var fileNameList = new List<string>();
 
-            if (Directory.Exists(_dirPath))
+            if (Directory.Exists(_relativeDirPath))
             {
-                fileNameList = Directory.GetFiles(_dirPath).ToList();
+                fileNameList = Directory.GetFiles(_relativeDirPath).ToList();
 
                 for (var i = 0; i < fileNameList.Count(); i++)
                 {
