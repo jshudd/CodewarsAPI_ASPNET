6bee1915c1f828a36ae4e9051475a195280f95fa
Jeremy <jshudd@gmail.com>
2024-01-09 16:45:52 -0600

    created new VsGroup Model

---
+++ CodewarsAPI_ASPNET/Models/VsGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CodewarsAPI_ASPNET.Models
+{
+	public class VsGroup
+	{
+		public VsGroup(string fileNameA, string fileNameB)
+		{
+			FileNameA = fileNameA;
+			FileNameB = fileNameB;
+		}
+
+		public string FileNameA { get; set; }
+        public string FileNameB { get; set; }
+
+		public Group GroupA
+		{
+			get;
+			set;
+		}
+
+		public Group GroupB { get; set; }
+	}
+}
+
