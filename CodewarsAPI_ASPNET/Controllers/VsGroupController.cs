887804d5526a29518806935717a8ce4c72311ce2
Jeremy <jshudd@gmail.com>
2024-03-29 16:30:52 -0500

    copied methods from UserController

--- CodewarsAPI_ASPNET/Controllers/VsGroupController.cs
+++ CodewarsAPI_ASPNET/Controllers/VsGroupController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using CodewarsAPI_ASPNET.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -28,11 +30,55 @@ namespace CodewarsAPI_ASPNET.Controllers
             return View(groupList);
         }
 
-        public IActionResult Versus(string fileNameA, string fileNameB)
+        public IActionResult Versus(VsGroup versus)
         {
+            versus.FileNameA = WebUtility.UrlDecode(versus.FileNameA);
+            versus.FileNameB = WebUtility.UrlDecode(versus.FileNameB);
 
+            var groupA = new Group();
+            var groupB = new Group();
 
-            return View();
+            var tempList = new List<User>();
+
+            var userNames = _csvRepo.ReadCsv(versus.FileNameA);
+
+            foreach (var user in userNames)
+            {
+                tempList.Add(_apiRepo.DeserializeJson(_apiRepo.CallApi(user).Result));
+            }
+
+            versus.GroupA = new Group(versus.FileNameA, tempList.OrderByDescending(x => x.Honor).ToList());
+
+            tempList.Clear();
+
+            userNames = _csvRepo.ReadCsv(versus.FileNameB);
+
+            foreach (var user in userNames)
+            {
+                tempList.Add(_apiRepo.DeserializeJson(_apiRepo.CallApi(user).Result));
+            }
+
+            versus.GroupB = new Group(versus.FileNameB, tempList.OrderByDescending(x => x.Honor).ToList());
+
+            return View(versus);
+        }
+
+        public Group GetGroup(string fileName)
+        {
+            fileName = WebUtility.UrlDecode(fileName);
+
+            var tempList = new List<User>();
+
+            var userNames = _csvRepo.ReadCsv(fileName);
+
+            foreach (var user in userNames)
+            {
+                tempList.Add(_apiRepo.DeserializeJson(_apiRepo.CallApi(user).Result));
+            }
+
+            var groupObj = new Group(fileName, tempList.OrderByDescending(x => x.Honor).ToList());
+
+            return groupObj;
         }
     }
 }
