c821d0d7782e6c6d1b9b9b418dfb388e6d6f8950
Jeremy <jshudd@gmail.com>
2024-03-29 15:49:59 -0500

    Created VsGroupController

---
+++ CodewarsAPI_ASPNET/Controllers/VsGroupController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodewarsAPI_ASPNET.Controllers
+{
+    public class VsGroupController : Controller
+    {
+        private readonly IApiRepo _apiRepo;
+        private readonly ICsvRepo _csvRepo;
+
+        public VsGroupController(IApiRepo apiRepo, ICsvRepo csvRepo)
+        {
+            _apiRepo = apiRepo;
+            _csvRepo = csvRepo;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<string> groupList = new List<string>();
+
+            groupList = _csvRepo.RetrieveCsvFileNames();
+
+            return View(groupList);
+        }
+
+        public IActionResult Versus(string fileNameA, string fileNameB)
+        {
+
+
+            return View();
+        }
+    }
+}
+
