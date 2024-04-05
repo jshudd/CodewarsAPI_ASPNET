8e0c5b7d35509406f94b9670a7fc9b09b6f7346e
Jeremy <jshudd@gmail.com>
2024-04-04 20:58:50 -0500

    Update AdminController.cs
    removed MS comment

--- CodewarsAPI_ASPNET/Controllers/AdminController.cs
+++ CodewarsAPI_ASPNET/Controllers/AdminController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using CodewarsAPI_ASPNET.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace CodewarsAPI_ASPNET.Controllers
 {
     public class AdminController : Controller
