8e36cb32d5b734259130257310f223d198cbd972
Jeremy <jshudd@gmail.com>
2024-04-04 20:45:10 -0500

    Create Admin.cs
    created Admin Model; added props

---
+++ CodewarsAPI_ASPNET/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CodewarsAPI_ASPNET.Models
+{
+	public class Admin
+	{
+		public Admin()
+		{
+		}
+
+		public bool IsCorrectPW { get; set; }
+		public string UserName { get; set; }
+		public string PW { get; set; }
+	}
+}
+
